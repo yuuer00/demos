c15b85913ed2bb9f265cd2ee523cc35337b1ef0a
yydu <863856587@qq.com>
2020-04-05 22:28:13 +0800

    element 级联菜单

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
@@ -16,7 +12,6 @@
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    init
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="d-flex">
+      <cascader-panel :options="options"></cascader-panel>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import cascaderPanel from "@/components/cascader-panel/cascader-panel";
+import { options } from "./data";
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: { cascaderPanel },
+  data() {
+    return {
+      options: options
+    };
   }
 };
 </script>
+<style lang="less" scoped>
+.d-flex {
+  display: flex;
+}
+</style>
