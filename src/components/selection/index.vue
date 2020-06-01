1bbb8277962e22f12f0c3e08a7bcc02e131f4eb2
刘帅鹏 <shuaipengliu@foxmail.com>
2020-06-01 18:11:43 +0800

    selection组件

---
+++ src/components/selection/index.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="selection">
+    <div class="selection_list">
+      <div
+        v-for="item in setting"
+        :key="item.id"
+        class="selection_item"
+        :class="{ active: isCheck(item.site) }"
+        @click="handleItemClick(item)"
+      >
+        <div class="selection_checkbox" @click.stop>
+          <el-checkbox
+            :value="isCheck(item.site)"
+            @input="handleItemClick(item)"
+          ></el-checkbox>
+        </div>
+
+        <div class="selection_content">
+          <div class="selection_title">{{ item.name }}</div>
+          <div class="selection_des">
+            {{ item.description }}
+          </div>
+        </div>
+        <div class="selection_child" v-if="item.site.length > 1" @click.stop>
+          <el-checkbox
+            v-for="child in item.site"
+            :key="child"
+            :value="isCheck(child)"
+            @input="handleChildChange(arguments[0], child)"
+            >{{ child }}
+          </el-checkbox>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ["setting"],
+  data() {
+    return {
+      checkSite: [],
+      // 目前选中的是否单选
+      isRadio: false
+    };
+  },
+  methods: {
+    // 传入字符串（'pcqq'）或数据（['pcqq','app']）
+    // 如果checkSite有任意一项，返回true，否则返回false
+    isCheck(site) {
+      if (typeof site === "string") {
+        return this.checkSite.indexOf(site) > -1;
+      }
+      return site.filter(item => this.checkSite.indexOf(item) > -1).length > 0;
+    },
+    handleItemClick(item) {
+      if (item.type === "0") {
+        if (this.isCheck(item.site)) {
+          this.checkSite = [];
+        } else {
+          this.checkSite = [].concat(item.site);
+        }
+        this.isRadio = true;
+      }
+      if (item.type === "1") {
+        if (this.isRadio) {
+          this.checkSite = [];
+        }
+        if (this.isCheck(item.site)) {
+          this.checkSite = this.checkSite.filter(
+            site => item.site.indexOf(site) < 0
+          );
+        } else {
+          this.checkSite = this.checkSite.concat(item.site);
+        }
+        this.isRadio = false;
+      }
+    },
+    handleChildChange(check, site) {
+      if (check) {
+        this.checkSite.push(site);
+      } else {
+        this.checkSite = this.checkSite.filter(item => item !== site);
+      }
+    }
+  },
+  watch: {
+    checkSite(val) {
+      this.$emit("change", val);
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.selection {
+  .selection_list {
+    .selection_item {
+      border-radius: 8px;
+      overflow: hidden;
+      border: 1px solid transparent;
+      .selection_checkbox {
+        float: left;
+        margin-left: 15px;
+        margin-top: 23px;
+      }
+      .selection_content {
+        padding: 20px 0 20px 40px;
+        cursor: pointer;
+        .selection_title {
+          font-weight: 600;
+          line-height: 25px;
+          color: #333;
+        }
+        .selection_des {
+          color: #999;
+          line-height: 40px;
+        }
+      }
+      .selection_child {
+        display: none;
+        line-height: 40px;
+        background: #ecf0f1;
+        padding-left: 40px;
+      }
+      &.active {
+        border: 1px solid #409eff;
+        .selection_child {
+          display: block;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <div class="d-flex">
+    <div class="d-flex" style="display:none;">
       <ducascader
         :options="options"
         v-model="checkedValue"
@@ -9,19 +9,62 @@
         :props="props"
       ></ducascader>
     </div>
-    {{ checkedValue }}
+    <!-- {{ checkedValue }} -->
+    <div style="margin: 20px auto;width:700px;">
+      <selection :setting="mock" @change="handleSelectionChange"></selection>
+    </div>
   </div>
 </template>
 
 <script>
+const mock = [
+  {
+    id: 0,
+    name: "微信公众号与小程序",
+    description: "基于微信公众号、小程序与小游戏生态的广告场景",
+    site: ["wechat"], // 数组长度大于1, 该项显示两个小选项
+    type: "0" // 1--多选， 0 单选
+  },
+  {
+    id: 1,
+    name: "QQ、腾讯看点、腾讯音乐",
+    description: "QQ、QQ空间、看点、浏览器、QQ音乐、全民K歌等媒体",
+    site: ["qq", "andro"],
+    type: "1" // 1--多选， 0 单选
+  },
+  {
+    id: 2,
+    name: "腾讯新闻与腾讯视频",
+    description: "腾讯新闻、腾讯视频、腾讯体育等媒体",
+    site: ["link", "ios"],
+    type: "1" // 1--多选， 0 单选
+  },
+  {
+    id: 3,
+    name: "优量汇",
+    description: "集合数万优质媒体海量曝光",
+    site: ["app"],
+    type: "1" // 1--多选， 0 单选
+  },
+  {
+    id: 4,
+    name: "PCQQ、QQ空间、腾讯音乐",
+    description: "PCQQ、QQ空间、腾讯音乐等媒体",
+    site: ["pcqq"],
+    type: "0" // 1--多选， 0 单选
+  }
+];
+
 // @ is an alias to /src
 import ducascader from "@/components/cascader-panel/index";
+import selection from "@/components/selection";
 import { options } from "./data";
 export default {
   name: "Home",
-  components: { ducascader },
+  components: { ducascader, selection },
   data() {
     return {
+      mock,
       checkedValue: [],
       options: options,
       props: { multiple: true, value: "id", label: "name", children: "child" },
@@ -37,6 +80,11 @@ export default {
         { title: "", multiple: true, isshowallcheck: true }
       ]
     };
+  },
+  methods: {
+    handleSelectionChange(val) {
+      console.log("handleSelectionChange", val);
+    }
   }
 };
 </script>
